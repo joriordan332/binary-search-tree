1a7b8ab16207392496e214e48c9b5c804063e9c8
Jonathan O'Riordan <jooriordan@hotmail.com>
2023-09-08 09:35:53 +0100

    Classes built & array duplicates removed & sorted
    Node class built that has attributes for data it stores and left and right children
    Tree class built which accepts array when initialised and has a root attribute which will be used in the buildTree function
    Array created which then has functions set up to remove duplicates and then a mergeSort function so that it is in order

---
+++ tree.js
@@ -0,0 +1,55 @@
+class Node {
+    constructor (data, left, right) {
+        this.data = data,
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Tree {
+    constructor (root) {
+        this.root;
+        this.array = [];
+    }
+}
+
+function buildTree() {
+
+}
+
+
+let array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+
+function removeDuplicates(array) {
+    return array.filter((item, index) => array.indexOf(item) === index)
+}
+
+const noDuplicates = removeDuplicates(array);
+
+function mergeSort(noDuplicates) {
+  if (noDuplicates.length < 2) {
+    return noDuplicates;
+  }
+
+  const mid = Math.floor(noDuplicates.length / 2);
+  const leftArray = noDuplicates.slice(0, mid);
+  const rightArray = noDuplicates.slice(mid);
+
+  return merge(mergeSort(leftArray), mergeSort(rightArray));
+}
+
+function merge(leftArray, rightArray) {
+  const mergeResult = [];
+  while (leftArray.length && rightArray.length) {
+    if (leftArray[0] <= rightArray[0]) {
+      mergeResult.push(leftArray.shift());
+    } else {
+      mergeResult.push(rightArray.shift());
+    }
+  }
+  return [...mergeResult, ...leftArray, ...rightArray];
+}
+
+const sortedArray = mergeSort(noDuplicates);
+array = sortedArray;
+console.log(array);
